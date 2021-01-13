00582509be65390fe86f550416a297079b926218
최영호 <bunirang@gmail.com>
2021-01-13 16:42:16 +0900

    First

---
+++ sample/reactNativeOpenCvTutorial/ios/OpenCV/RNOpenCvLibrary.h
@@ -0,0 +1,12 @@
+//
+//  RNOpenCvLibrary.h
+//  reactNativeOpenCvTutorial
+//
+//  Created by young ho choi on 2020/12/07.
+//
+
+#ifndef RNOpenCvLibrary_h
+#define RNOpenCvLibrary_h
+
+
+#endif /* RNOpenCvLibrary_h */
---
+++ sample/reactNativeOpenCvTutorial/ios/OpenCV/RNOpenCvLibrary.m
@@ -0,0 +1,8 @@
+//
+//  RNOpenCvLibrary.m
+//  reactNativeOpenCvTutorial
+//
+//  Created by young ho choi on 2020/12/07.
+//
+
+#import <Foundation/Foundation.h>
